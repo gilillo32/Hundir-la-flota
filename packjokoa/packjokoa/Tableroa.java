162886e6f15f9d3de1fe816c9a8b2cefae06d568
leire <leire@DESKTOP-98AEAF4>
2020-03-23 20:00:44 +0100

    hemos implementado tableroa bete  eta imprimatu

--- packjokoa/packjokoa/Tableroa.java
+++ packjokoa/packjokoa/Tableroa.java
@@ -6,21 +6,48 @@ public class Tableroa {
 	private String[][] matrizea;
 	
 	//eraikitzailea:
-	public Tableroa(int pTamaina) {
-		
+	public Tableroa() { /*BIGARREN MAILAKO ATAZA  int pTamaina*/
+		this.errenZutKop = 10;
+		this.matrizea = new String[this.errenZutKop][this.errenZutKop];
 	}
 	
 	//gainontzeko metodoak:
 	
 	public void tableroaInprimatu() {
+		//hasieratu indizeak
+		int e= 0;
+		int z= 0;
+		System.out.print(" "); // lehennegoz hutsune bat inprimatu
+		//Orain zutabeen indizeak imprimatuko ditu
+		while (e<this.errenZutKop + 1) { 
+			System.out.print(" "+e);	
+			//deasberdin egingo dugu luzera hamar baino handiagoa denean, bain ahori bigarren mailako ataza da.
+		}
+		e=0; //berriro hasieratuko dugu
+		while (e< this.errenZutKop + 1) { //erabili daiteke This.matrizea.length
+			while (z<this.errenZutKop+1) { //erabili daiteke this.matrize[e].length
+				System.out.print(e+ " " + this.matrizea[e][z]);				
+			}			
+		}
 		
 	}
 	
-	public void tableroaBete() {
+	public void tableroaBete() { //uraz beteko dugu "-"
+		//hasieratu indizeak
+		int e= 0;
+		int z= 0;
+
+		while (e< this.errenZutKop + 1) { //erabili daiteke This.matrizea.length
+			while (z<this.errenZutKop+1) { //erabili daiteke this.matrize[e].length
+				this.matrizea[e][z] = "-";				
+			}			
+		}
+		
 		
 	}
 	
 	public void itsasontziakJarri() {
+		
 		
 	}
 	
