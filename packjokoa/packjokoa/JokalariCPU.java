f54e76d2f844d6a7f4410fc0a4bdaa4d71c223b3
Iñigo <inigogil@bilbokoeskolapioak.org>
2020-03-24 10:26:04 +0100

    Commit de por si acaso para no perder el progreso.

--- packjokoa/packjokoa/JokalariCPU.java
+++ packjokoa/packjokoa/JokalariCPU.java
@@ -1,26 +1,59 @@
 package packjokoa;
+import java.util.*;
 
 public class JokalariCPU extends Jokalaria {
 	
-	public JokalariCPU() {
-		
+	public JokalariCPU(short pErrenkadaZutKop) {
+		super("CPU", pErrenkadaZutKop);
+	}
+	
+	@Override 
+	public void txandaBatJokatu(JokalariArrunta pJokalaria) {
+		boolean posibleaDa = true;
+		boolean aurrekoanUkituDu = false;
+		short jarraianAurkituDitzakeenUkitutak = 4;
+		short zenbatUkituDituJarraian = 0;
+		Random rand = new Random();
+		short pX = (short) ((short) rand.nextInt(10) + 1);
+		short pY = (short) ((short) rand.nextInt(10) + 1);
+		while(posibleaDa) { 
+			//Ura ukitzen duen arte errepikatuko den buklea:
+			if(this.lehenengoTiroaEgin(pX, pY)) {
+				String emaitza = pJokalaria.bigarrenTiroaEgin(pX, pY);
+				this.hirugarrenTiroaEgin(pX, pY, emaitza);
+				if(emaitza != "U") {
+					//Itsasontzia ez badu ukitzen:
+					posibleaDa = false;
+				}
+				aurrekoanUkituDu = true;
+			}
+			else {
+				//Tiroa ez bada posiblea:
+				posibleaDa = false;
+			}
+		}
 	}
 
 	@Override
 	public void lehenengoTiroaEgin(short pX, short pY) {
-		// TODO Auto-generated method stub
+		return super.getNireTableroa().konprobatuTiroa(pX, pY);
 	}
 
 	@Override
 	public String bigarrenTiroaEgin(short pX, short pY) {
-		// TODO Auto-generated method stub
-		return null;
+		return super.getNireTableroa().bigarrenTiroaEgin(pX, pY);
+		//itzuliko duen String-a hirugarren tiroan erabiliko da
 	}
 
 	@Override
 	public void hirugarrenTiroaEgin(short pX, short pY, String pEma) {
-		// TODO Auto-generated method stub
 		// primeros llamamos a super.nUkituaInkrementatu y luego a tableroa.eguneratu
+		//bigarren tiroan itzuli duen String-a dagokion posizioan sartuko du
+		if(pEma == "U") {
+			//Itsasontzia "U"kitu badu:
+			super.nUkituaInkrementatu();
+		}
+		this.getNireTableroa().eguneratuTableroa(pEma);
 
 	}
 	
@@ -29,7 +62,7 @@ public class JokalariCPU extends Jokalaria {
 	}
 	
 	public boolean itsasontzirikEz() {
-		super();
+		return super.itsasontzirikEz();
 	}
 	
 	
