6718859b873073d7393c32812e81ccad0e1904b6
leire <leire@DESKTOP-98AEAF4>
2020-03-26 12:53:46 +0100

    jdt

--- packjokoa/salbuespenak/KoordenatuEzEgokiak.java
+++ packjokoa/salbuespenak/KoordenatuEzEgokiak.java
@@ -1,7 +1,10 @@
 package salbuespenak;
 
 public class KoordenatuEzEgokiak extends Exception {
+	//eraikitzaaileak
+		//parametrorik gabe
 	public KoordenatuEzEgokiak() {}
+		//parametroekin
 	public KoordenatuEzEgokiak() {
 		System.out.println("Sartu dituzun kordenatuak ez dira egkiak, mesedez sartu koordenatuak berriro.");
 	}
