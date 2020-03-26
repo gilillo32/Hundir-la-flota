60d07ee203c8c48659637c8757a806e7b14c96ff
gilillo32 <35781852+gilillo32@users.noreply.github.com>
2020-03-26 12:48:49 +0100

    Clase Koordenatuak añadida

---
+++ packjokoa/packjokoa/Koordenatuak.java
@@ -0,0 +1,22 @@
+package packjokoa;
+
+public class Koordenatuak {
+	
+	private short x;
+	private short y;
+	
+	public Koordenatuak(short pX, short pY) {
+		this.x = pX;
+		this.y = pY;
+	}
+	
+	public void setKoordenatuak(short pX, short pY) {
+		this.x = pX;
+		this.y = pY;
+	}
+	
+	public Koordenatuak getKoordenatuak() {
+		return this;
+	}
+
+}
