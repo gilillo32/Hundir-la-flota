2e66f66f755fbe5a6e27214a0f1f95b4f4fb4d33
leire <leire@DESKTOP-98AEAF4>
2020-04-11 11:11:54 +0200

    KoordenatuakTest eginDA JEJEJEJJ

--- packjokoa/probak/KoordenatuakTest.java
+++ packjokoa/probak/KoordenatuakTest.java
@@ -6,44 +6,63 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import packjokoa.Koordenatuak;
+
 public class KoordenatuakTest {
+	private Koordenatuak k, k1;
 
 	@Before
 	public void setUp() throws Exception {
+		k = new Koordenatuak();
+		k1 = new Koordenatuak((short)1, (short)1);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		k=null;
+		k1=null;
 	}
 
 	@Test
 	public void testKoordenatuak() {
-		fail("Not yet implemented");
+		assertNotNull(k);
 	}
 
 	@Test
 	public void testKoordenatuakShortShort() {
-		fail("Not yet implemented");
+		assertNotNull(k1);
 	}
 
 	@Test
 	public void testSetKoordenatuakX() {
-		fail("Not yet implemented");
+		short x = k.getKoordenatuakX();		
+		assertEquals((short)-1, x); //hasierako balioa		
+		
+		k.setKoordenatuakX((short) 2);
+		x = k.getKoordenatuakX();	
+		assertEquals((short)2, x);
 	}
 
 	@Test
 	public void testGetKoordenatuakX() {
-		fail("Not yet implemented");
+		short x = k1.getKoordenatuakX();		
+		assertEquals((short)1, x); //hasierako balioa		
 	}
 
 	@Test
 	public void testSetKoordenatuakY() {
-		fail("Not yet implemented");
+		short y = k.getKoordenatuakY();
+		assertEquals((short)-1, y);//hasierako balioa
+		
+		k.setKoordenatuakY((short) 2);
+		y = k.getKoordenatuakY();
+		assertEquals((short)2, y);
 	}
 
 	@Test
 	public void testGetKoordenatuakY() {
-		fail("Not yet implemented");
+		short y = k1.getKoordenatuakY();
+		assertEquals((short)1, y);
 	}
 
 }
