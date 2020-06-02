8bf598e0763ae79bec014aa2541682330513e511
leire <leire@DESKTOP-98AEAF4>
2020-06-02 20:40:34 +0200

    f

--- packjokoa/klaseak/BikoteJokalariak.java
+++ packjokoa/klaseak/BikoteJokalariak.java
@@ -119,7 +119,6 @@ public class BikoteJokalariak {
 		boolean aurrekoanAsmatu =false;
 		Koordenatuak k1= new Koordenatuak();
 		
-	//JokalariArrunta:
 			System.out.println(" ");
 			System.out.println(" ");
 			if(this.norenKontra.equals("CPU")) {
@@ -168,6 +167,7 @@ public class BikoteJokalariak {
 				}input_lehenengo.close();
 				this.zerrenda[0].getPrintTableroa().tableroaInprimatu();
 			}
+//JokalariArrunta:
 		do {
 			Koordenatuak k= ((JokalariArrunta)this.getZerrenda()[0]).koordenatuaAukeratu();
 			short pX= (short) (k.getKoordenatuakX() + 1);
@@ -209,7 +209,7 @@ public class BikoteJokalariak {
 			}
 			
 		}while(posibleaDa);
-		
+//Bigarren jokalaria		
 	switch(this.norenKontra) {
 	case "CPU":
 		//JokalariCPU 
